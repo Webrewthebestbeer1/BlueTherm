356bf64f6c1780579bf759cb2572d77a1ff5af90
Iver Egge <iverasp@gmail.com>
2016-04-22 10:00:47 +0200

    alert dialog when no bluetooth device present

--- app/src/main/java/no/iegget/bluetherm/MainActivity.java
+++ app/src/main/java/no/iegget/bluetherm/MainActivity.java
@@ -1,12 +1,15 @@
 package no.iegget.bluetherm;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,9 +37,21 @@ public class MainActivity extends AppCompatActivity {
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
-            // TODO: alert user that this app will not run on the device
+            Log.w("MainActivity", "no bluetooth available");
+            new AlertDialog.Builder(this)
+                    .setTitle("No Bluetooth available")
+                    .setMessage("This app only works on devices with Bluetooth")
+                    .setCancelable(false)
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            exitApplication();
+                        }
+                    })
+                    .show();
+
         }
-        if (!mBluetoothAdapter.isEnabled()) {
+        else if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
@@ -47,6 +62,11 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void exitApplication() {
+        this.finish();
+        System.exit(0);
     }
 
     @Override
